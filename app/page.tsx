a6f2297cebbaedf0ff8c52642809a9417e88b287
Prashant Singh <prashantdineshsingh48@gmail.com>
2025-12-26 20:08:11 +0530

    Setup database

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="text-4xl font-bold text-green-500">
-      Hello world!
-    </div>
+    <Button variant='destructive'>Click Me</Button>
   );
 }
