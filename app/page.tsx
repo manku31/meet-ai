2965349ed0d6868baf042b52bf93c17690bff385
Prashant Singh <prashantdineshsingh48@gmail.com>
2025-12-26 22:34:23 +0530

    Setup better auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,101 @@
+"use client";
+
+import { useState } from "react";
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 
 export default function Home() {
+  // State variables
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { data: session } = authClient.useSession();
+
+  // Function to handle form submission
+  const onSubmit = async () => {
+    const { data, error } = await authClient.signUp.email({
+      name,
+      email,
+      password,
+    });
+
+    if (error) {
+      console.error("Error creating user:", error);
+      window.alert(`Something went wrong`);
+      return;
+    }
+
+    if (data) {
+      console.log("User created successfully:", data);
+      window.alert(`User created successfully: ${data.user.email}`);
+      return;
+    }
+  };
+
+  const onLogin = async () => {
+    const { data, error } = await authClient.signIn.email({
+      email,
+      password,
+    });
+
+    if (error) {
+      console.error("Error logging in:", error);
+      window.alert(`Something went wrong`);
+      return;
+    }
+
+    if (data) {
+      console.log("User logged in successfully:", data);
+      window.alert(`User logged in successfully`);
+      return;
+    }
+  };
+
   return (
-    <Button variant='destructive'>Click Me</Button>
+    <div>
+      {session ? (
+        <div>
+          <h1>Logged in as {session.user.email}</h1>
+          <Button onClick={() => authClient.signOut()}>Sign out</Button>
+        </div>
+      ) : (
+        <div className="grid grid-row-3 gap-4">
+          <div className="p-4 flex flex-col gap-y-4">
+            <Input
+              placeholder="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Input
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={onSubmit}>Create User</Button>
+          </div>
+          <div className="p-4 flex flex-col gap-y-4">
+            <Input
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={onLogin}>Login</Button>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
